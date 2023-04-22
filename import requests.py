121193046d1ca4d5e4a838c3a542c73f844a9148
Jyoti Maharjan <jmaharja@uci.edu>
2023-04-22 09:21:38 -0700

    Initial query running

---
+++ import requests.py
@@ -0,0 +1,20 @@
+import requests
+APIKEY = 'Bearer vIJp-pW2ISoT0G0LVHUpdwhRfESBy_W5Wl53c2kpoalMOEIATNBkxNK_9ZNmn1IOjo6fG9Kb6tHxoGZJGl61DgB5FfkvgOj8dKIPNIMxsqMMAPGoLGVbXCuzn_NDZHYx'
+
+priceLevel = input('number of $!')
+
+url = f"https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20&location=LA&price={priceLevel}"
+
+#zip_code = input('Enter the zip code :')
+
+
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "Authorization": APIKEY
+    #"postal_code":zip_code
+
+}
+
+response = requests.get(url, headers=headers)
+print(response.text)
