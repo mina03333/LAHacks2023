c057925e1074f50e8a965ecaac3e11a0ee484abf
Mina Yoon <ijhamin0320@gmail.com>
2023-04-22 10:13:02 -0700

    Fixed some minor errors

--- get_user_input.py
+++ get_user_input.py
@@ -1,9 +1,12 @@
-function get_location():
+def get_location():
     location = input("Enter your location: ")
+    return location
 
-function get_activity():
+
+def get_activity():
     is_activites_valid = False
-    while !is_activities_valid:
+    activities = None
+    while not is_activities_valid:
         print("1. temp activity")
         print("2. temp activity")
         print("3. temp activity")
@@ -16,13 +19,19 @@ function get_activity():
         elif input > 5 or input < 1:
             print("Please enter a valid number.")
         else:
-            is_activities_valid = True;
+            is_activities_valid = True
             break
 
-function get_distance():
+    return activities
+
+
+def get_distance():
+    distance = None
     while True:
         distance = input("Enter distance in miles: ")
         if distance > 30:
             pass
         else:
             break
+
+    return distance
