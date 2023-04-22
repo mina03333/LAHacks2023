efd865c7aaded64d6027cf53e7f7b404dc51a48a
Mina Yoon <ijhamin0320@gmail.com>
2023-04-22 10:34:05 -0700

    Checking for user input

---
+++ main_module.py
@@ -0,0 +1,11 @@
+from get_user_input import get_location, get_activity, get_distance
+
+
+def main() -> None:
+    location = get_location()
+    activity = get_activity()
+    distance = get_distance()
+
+
+if __name__ == '__main__':
+    main()
