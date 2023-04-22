3d0df0b68bac548d7c7f4baa451c111a02940740
Jyoti Maharjan <jmaharja@uci.edu>
2023-04-22 10:22:17 -0700

    set api url based on get_user_input fuction inputs

--- SortAPI.py
+++ SortAPI.py
@@ -4,7 +4,7 @@ import get_user_input
 APIKEY = 'Bearer vIJp-pW2ISoT0G0LVHUpdwhRfESBy_W5Wl53c2kpoalMOEIATNBkxNK_9ZNmn1IOjo6fG9Kb6tHxoGZJGl61DgB5FfkvgOj8dKIPNIMxsqMMAPGoLGVbXCuzn_NDZHYx'
 
 
-url = f"https://api.yelp.com/v3/businesses/search?location={location}&latitude={latitude}&longitude={longitude}&term={term}&radius={radius}&categories={cat1}&categories={cat2}&sort_by=distance&limit={limit}"
+url = f"https://api.yelp.com/v3/businesses/search?location={get_user_input.get_location}&term={get_user_input.get_activity}&radius={get_user_input.get_distance}&sort_by=distance&limit={20}"
 
 #API call
 headers = {
