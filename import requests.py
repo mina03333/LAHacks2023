1156c3f5908a3d0ba8495e85bbf0dd50d31aa60f
Tiffany Cheung <Cheungt6@uci.edu>
2023-04-22 10:06:58 -0700

    added the search words into website

--- import requests.py
+++ import requests.py
@@ -1,9 +1,10 @@
 import requests
 APIKEY = 'Bearer vIJp-pW2ISoT0G0LVHUpdwhRfESBy_W5Wl53c2kpoalMOEIATNBkxNK_9ZNmn1IOjo6fG9Kb6tHxoGZJGl61DgB5FfkvgOj8dKIPNIMxsqMMAPGoLGVbXCuzn_NDZHYx'
 
-priceLevel = input('number of $!')
+priceLevel = input('number of $!').strip()
+search_words = input("Enter any acitivity that you want to do:\n").strip().replace(" ", "_")
 
-url = f"https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20&location=LA&price={priceLevel}"
+url = f"https://api.yelp.com/v3/businesses/search?attributes={search_words}sort_by=best_match&limit=20&location=LA&price={priceLevel}"
 
 #zip_code = input('Enter the zip code :')
 
@@ -12,7 +13,7 @@ headers = {
     "accept": "application/json",
     "content-type": "application/json",
     "Authorization": APIKEY
-
+    #"postal_code":zip_code
 }
 
 response = requests.get(url, headers=headers)
