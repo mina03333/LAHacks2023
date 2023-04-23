cf19346ca7045cfaf0cb7152e4c7996741b97cbd
Jyoti Maharjan <jmaharja@uci.edu>
2023-04-22 20:58:22 -0700

    format output from cohere properly

--- itinerary_recommendations.py
+++ itinerary_recommendations.py
@@ -5,7 +5,7 @@ co = cohere.Client('3oLfmlBPNNCZBWB008008nFdhJ8Z6rDPCnwRDNyo')
 def itinerary(info):
     response = co.generate(
         model='command-xlarge-nightly',
-        prompt=f'Using this information, create an itinerary for the day:{info}',
+        prompt=f'Using this information, create an itinerary for the day:{info}. Format the results as:(time) - (name)\n\tPrice Range: (price range)\n\tLocation: (location)\n\tServices: (services)',
         max_tokens=1432,
         temperature=0.9,
         k=0,
